7a3e37fd5e58542406a06b478688dcd2790df1e5
Nathan Lee <2431820-nathanal@users.noreply.gitlab.com>
2022-12-12 12:49:29 +1100

    Fix crash deleting row, Selectors dialog on Windows
    _deletion doesn't seem necessary (only used in one
    place). I've removed that use, replacing it with a
    defensive check to avoid crashing, though we might
    end up selecting a different cell now. Now _deletion
    is only true during the call to _onPropDelete, and is
    used to stop readStyleElement from running and deleting
    the tree view, which is still used by a caller of
    _onPropDelete (gtk_tree_view_multipress_gesture_pressed).
    
    Fix https://gitlab.com/inkscape/inkscape/-/issues/3986

--- src/ui/dialog/styledialog.cpp
+++ src/ui/dialog/styledialog.cpp
@@ -377,7 +377,7 @@ void StyleDialog::readStyleElement()
     g_debug("StyleDialog::readStyleElement");
 
     auto document = getDocument();
-    if (_updating || !document)
+    if (_updating || !document || _deletion)
         return; // Don't read if we wrote style element.
     _updating = true;
     _scrollock = true;
@@ -988,6 +988,7 @@ void StyleDialog::_onPropDelete(Glib::ustring path, Glib::RefPtr<Gtk::TreeStore>
         store->erase(row);
         _deletion = true;
         _writeStyleElement(store, selector);
+        _deletion = false;
     }
 }
 
@@ -1230,7 +1231,6 @@ void
 StyleDialog::_startValueEdit(Gtk::CellEditable* cell, const Glib::ustring& path, Glib::RefPtr<Gtk::TreeStore> store)
 {
     g_debug("StyleDialog::_startValueEdit");
-    _deletion = false;
     _scrollock = true;
     Gtk::TreeModel::Row row = *store->get_iter(path);
     if (row) {
@@ -1290,7 +1290,6 @@ StyleDialog::_startValueEdit(Gtk::CellEditable* cell, const Glib::ustring& path,
 
 void StyleDialog::_startNameEdit(Gtk::CellEditable *cell, const Glib::ustring &path)
 {
-    _deletion = false;
     g_debug("StyleDialog::_startNameEdit");
     _scrollock = true;
     Glib::RefPtr<Gtk::ListStore> completionModel = Gtk::ListStore::create(_mCSSData);
@@ -1314,14 +1313,15 @@ void StyleDialog::_startNameEdit(Gtk::CellEditable *cell, const Glib::ustring &p
 gboolean sp_styledialog_store_move_to_next(gpointer data)
 {
     StyleDialog *styledialog = reinterpret_cast<StyleDialog *>(data);
-    if (!styledialog->_deletion) {
-        auto selection = styledialog->_current_css_tree->get_selection();
-        Gtk::TreeIter iter = *(selection->get_selected());
-        Gtk::TreeModel::Path model = (Gtk::TreeModel::Path)iter;
-        if (model == styledialog->_current_path) {
-            styledialog->_current_css_tree->set_cursor(styledialog->_current_path, *styledialog->_current_value_col,
-                                                       true);
-        }
+    auto selection = styledialog->_current_css_tree->get_selection();
+    Gtk::TreeIter iter = *(selection->get_selected());
+    if (!iter) {
+        return FALSE;
+    }
+    Gtk::TreeModel::Path model = (Gtk::TreeModel::Path)iter;
+    if (model == styledialog->_current_path) {
+        styledialog->_current_css_tree->set_cursor(styledialog->_current_path, *styledialog->_current_value_col,
+                                                   true);
     }
     return FALSE;
 }
