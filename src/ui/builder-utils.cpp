05a6845c6c4665aba7cb3f8370e6df67d6595aac
Daniel Boles <dboles.src+inkscape@gmail.com>
2023-07-07 13:09:11 +0100

    ui/builder-utils: Include object ID in throw. etc.
    Also add missing header/footer, some includes, &c.

--- src/ui/builder-utils.cpp
+++ src/ui/builder-utils.cpp
@@ -1,9 +1,37 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+/** @file
+ * @brief Gtk builder utilities
+ */
+/* Authors:
+ *   Michael Kowalski <michal_kowalski@hotmail.com>
+ *   Daniel Boles <dboles.src+inkscape@gmail.com>
+ *
+ * Copyright (C) 2021 Michael Kowalski <michal_kowalski@hotmail.com>
+ * Copyright (C) 2023 Daniel Boles
+ * Released under GNU GPL v2+, read the file 'COPYING' for more information.
+ */
+
+#include <stdexcept>
+#include <glibmm/error.h>
+#include <glibmm/ustring.h>
+
 #include "builder-utils.h"
 #include "io/resource.h"
 
 namespace Inkscape {
 namespace UI {
+
+namespace Detail {
+
+void throw_missing(const char* object_type, const char* id)
+{
+    // TODO: GTK4: Include builder->get_current_object()
+    auto what = Glib::ustring::compose(
+        "Missing %1 `%2` in Gtk::Builder glade/ui resource file", object_type, id);
+    throw std::runtime_error{what.raw()};
+}
+
+} // namespace Detail
 
 Glib::RefPtr<Gtk::Builder> create_builder(const char* filename) {
     auto glade = Inkscape::IO::Resource::get_filename(Inkscape::IO::Resource::UIS, filename);
@@ -17,4 +45,15 @@ Glib::RefPtr<Gtk::Builder> create_builder(const char* filename) {
     }
 }
 
-}} // namespace
+} } // namespace Inkscape::UI
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace .0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim:filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99:
--- src/ui/builder-utils.h
+++ src/ui/builder-utils.h
@@ -3,27 +3,38 @@
  * @brief Gtk builder utilities
  */
 /* Authors:
- *   Michael Kowalski
+ *   Michael Kowalski <michal_kowalski@hotmail.com>
+ *   Daniel Boles <dboles.src+inkscape@gmail.com>
  *
  * Copyright (C) 2021 Michael Kowalski
+ * Copyright (C) 2023 Daniel Boles
  * Released under GNU GPL v2+, read the file 'COPYING' for more information.
  */
 
 #ifndef SEEN_BUILDER_UTILS_H
 #define SEEN_BUILDER_UTILS_H
 
+#include <utility>
 #include <glibmm/refptr.h>
 #include <gtkmm/builder.h>
 
 namespace Inkscape {
 namespace UI {
 
+// load glade file from share/ui folder and return builder; throws on errors
+Glib::RefPtr<Gtk::Builder> create_builder(const char* filename);
+
+namespace Detail {
+// Throw a std::runtime_error with text explaining that a widget/object is missing & its ID.
+void throw_missing(const char* object_type, const char* id);
+} // namespace Detail
+
 // get widget from builder or throw
 template<class W> W& get_widget(const Glib::RefPtr<Gtk::Builder>& builder, const char* id) {
     W* widget;
     builder->get_widget(id, widget);
     if (!widget) {
-        throw std::runtime_error("Missing widget in a glade resource file");
+        Detail::throw_missing("widget", id);
     }
     return *widget;
 }
@@ -33,7 +44,7 @@ W& get_derived_widget(const Glib::RefPtr<Gtk::Builder>& builder, const char* id,
     W* widget;
     builder->get_widget_derived(id, widget, std::forward<Args>(args)...);
     if (!widget) {
-        throw std::runtime_error("Missing widget in a glade resource file");
+        Detail::throw_missing("widget", id);
     }
     return *widget;
 }
@@ -41,7 +52,7 @@ W& get_derived_widget(const Glib::RefPtr<Gtk::Builder>& builder, const char* id,
 template<class Ob> Glib::RefPtr<Ob> get_object(Glib::RefPtr<Gtk::Builder>& builder, const char* id) {
     auto object = Glib::RefPtr<Ob>::cast_dynamic(builder->get_object(id));
     if (!object) {
-        throw std::runtime_error("Missing object in a glade resource file");
+        Detail::throw_missing("object", id);
     }
     return object;
 }
@@ -55,14 +66,22 @@ Ob &get_object_raw(Glib::RefPtr<Gtk::Builder> &builder, const char *id)
 {
     auto object = dynamic_cast<Ob *>(builder->get_object(id).get());
     if (!object) {
-        throw std::runtime_error("Missing object in a glade resource file");
+        Detail::throw_missing("object", id);
     }
     return *object;
 }
 
-// load glade file from share/ui folder and return builder; throws on errors
-Glib::RefPtr<Gtk::Builder> create_builder(const char* filename);
-
 } } // namespace
 
 #endif // SEEN_BUILDER_UTILS_H
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace .0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim:filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99:
