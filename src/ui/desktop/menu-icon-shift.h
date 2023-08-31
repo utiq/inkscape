2886f592109d876750b685c286187a764612285e
Daniel Boles <dboles.src@gmail.com>
2023-08-31 13:41:56 +0000

    Export: Tweak UI, − ButtonBox | menu-icon-shift: Fix too much spacing/misaligned
    commit c3dd01fcc188f1ee19aa005ec67571e763dc47c2 (HEAD -> djb_Export_ui_fixes)
    Author: Daniel Boles <dboles.src+inkscape@gmail.com>
    Date:   Wed Aug 30 12:07:38 2023 +0100
    
        menu-icon-shift: Fix too much spacing = misaligned
        
        Itʼs not clear why the spacing we add was multiplied by 1.5, but at
        least now, that seems wrong and means menu items with icons shifted too
        far. Fix that by removing the multiplication by 1.5
        
        Also, exit early once we have shifted: no point continuing on / redoing.
    
    commit 18463af29d1c8aa7521ac5b306e68592fd68e848
    Author: Daniel Boles <dboles.src+inkscape@gmail.com>
    Date:   Wed Aug 30 11:29:16 2023 +0100
    
        Export: Tweak UI consistency/tidiness, − ButtonBox
        
        * Add margin below Single File tab, like Batch Export already had
        * Use Title Case for some labels missing it (but WAS used in other tab)
        * Remove margin before filename entry on Single File tab to match Batch
        * Add tooltip on filename entry in Batch Export, already in Single File
        * Replace GtkButtonBox with GtkBox as the former does not exist in GTK4
        * Add tooltip to Format Options button
        * Suppress frame in its popover's viewport as adds nothing / looks ugly

--- src/ui/desktop/menu-icon-shift.h
+++ src/ui/desktop/menu-icon-shift.h
@@ -1,7 +1,4 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-#ifndef SEEN_DESKTOP_MENU_ITEM_SHIFT_H
-#define SEEN_DESKTOP_MENU_ITEM_SHIFT_H
-
 /**
  * @file
  * Shift Gtk::MenuItems with icons to align with Toggle and Radio buttons.
@@ -10,21 +7,22 @@
  * Authors:
  *   Tavmjong Bah       <tavmjong@free.fr>
  *   Patrick Storz      <eduard.braun2@gmx.de>
+ *   Daniel Boles       <dboles.src+inkscape@gmail.com>
  *
- * Copyright (C) 2020 Authors
+ * Copyright (C) 2020-2023 Authors
  *
  * The contents of this file may be used under the GNU General Public License Version 2 or later.
  * Read the file 'COPYING' for more information.
  *
  */
 
-namespace Gtk {
-  class MenuShell;
-}
+#ifndef SEEN_DESKTOP_MENU_ITEM_SHIFT_H
+#define SEEN_DESKTOP_MENU_ITEM_SHIFT_H
 
-/**
- * Call back to shift icons into place reserved for toggles (i.e. check and radio items).
- */
+namespace Gtk {
+class MenuShell;
+} // namespace Gtk
+
 bool shift_icons(Gtk::MenuShell *menu);
 
 #endif // SEEN_DESKTOP_MENU_ITEM_SHIFT_H
